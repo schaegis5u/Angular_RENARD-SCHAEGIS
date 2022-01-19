54592c9631c8310f41dbf7e18acfc6998931d2dd
Aurélien <70693113+schaegis5u@users.noreply.github.com>
2022-01-19 10:19:45 +0100

    Création des composants principaux

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,15 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import { ToDoListComponent } from './shared/component/to-do-list/to-do-list.component';
+import { ToDoComponent } from './to-do/to-do.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToDoListComponent,
+    ToDoComponent,
   ],
   imports: [
     BrowserModule,
