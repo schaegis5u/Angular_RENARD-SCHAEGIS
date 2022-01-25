ce36e7375798250714ea83e776b6f1a0cc2d6642
Aurélien <70693113+schaegis5u@users.noreply.github.com>
2022-01-25 16:58:58 +0100

    impossible de créer une tâche vide

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -4,45 +4,51 @@ import { PromiseRequest } from '../shared/services/promise/promise-request';
 import { Router, RouterModule, Routes } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
-
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+  styleUrls: ['./form.component.scss'],
 })
 export class FormComponent implements OnInit {
-
   Form!: FormGroup;
   public submitted = false;
 
-  constructor(public promise: PromiseRequest, private formBuilder : FormBuilder, private router : Router, private _snackBar : MatSnackBar){}
+  constructor(
+    public promise: PromiseRequest,
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private _snackBar: MatSnackBar
+  ) {}
 
   ngOnInit(): void {
     this.Form = this.formBuilder.group({
       tache: ['', Validators.required],
-    })
-  }
-
-  get f() { return this.Form.controls; }
-
-  public onSubmit() : void{
-    let tache = this.Form.controls['tache'].value
-    let tabactuel = JSON.parse(localStorage.getItem("classifie") || '[]');
-    tabactuel.push(tache);
-    localStorage.setItem('classifie', JSON.stringify(tabactuel));
-    
-  }
-
-  retour() : void {
-    this.router.navigate(['/list/']);
-  }
-
-  snackBar() : void {
-    this._snackBar.open('Tâche ajoutée', 'OK', {
-      horizontalPosition: "center",
-      verticalPosition: "top",
-      duration: 1000,
     });
   }
 
+  get f() {
+    return this.Form.controls;
+  }
+
+  public onSubmit(): void {
+    let tache = this.Form.controls['tache'].value;
+    let tabactuel = JSON.parse(localStorage.getItem('classifie') || '[]');
+    tabactuel.push(tache);
+    if (tache.length != 0) {
+      localStorage.setItem('classifie', JSON.stringify(tabactuel));
+      this.snackBar();
+    }
+  }
+
+  retour(): void {
+    this.router.navigate(['/list/']);
+  }
+
+  snackBar(): void {
+    this._snackBar.open('Tâche ajoutée', 'OK', {
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+      duration: 1000,
+    });
+  }
 }
